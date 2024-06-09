b2940f9363503fcc24856711983a49590a50e882
Giordi29 <157144092+Giordi29@users.noreply.github.com>
2024-06-09 19:52:01 +0000

    First commit

---
+++ simple-counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import SimpleCounter from "./SimpleCounter"
+import calculateSeconds from "./lib/time";
+
+function App() {
+const [counter, setCounter] = useState (0);
+
+useEffect(() => {
+  const interval = setInterval(() => {
+    setCounter(counter => counter + 1)
+  }, 1000)
+
+  return () => clearInterval(interval)
+}, [counter])
+
+
+
+  return (
+    <>
+      <SimpleCounter 
+      thousandsDigit = {calculateSeconds(counter, 1000)}
+      hundredsDigit = {calculateSeconds(counter, 100)}
+      tensDigit = {calculateSeconds(counter, 10)}
+      onesDigit = {calculateSeconds(counter, 1)}
+      />
+      
+    </>
+  )
+}
+
+export default App
